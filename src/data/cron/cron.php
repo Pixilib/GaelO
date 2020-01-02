f4d5edf67982cdf800aad884055756ed86f0aabb
Salim Kanoun <salim.kanoun@gmail.com>
2020-01-02 23:44:13 +0100

    new logs

--- src/data/cron/cron.php
+++ src/data/cron/cron.php
@@ -1,3 +1,20 @@
 <?php 
-error_log('cronStarted');
+
+/**
+ * This files is called from the crontab container every minute
+ */
+
+require_once(__DIR__.'/../../vendor/autoload.php');
+
+echo('Cron started');
+
+use GO\Scheduler;
+
+// Create a new scheduler
+$scheduler = new Scheduler();
+
+$scheduler->php('import_patient.php')->sunday(23, 50);
+
+// Let the scheduler execute jobs which are due.
+$scheduler->run();
 ?>
