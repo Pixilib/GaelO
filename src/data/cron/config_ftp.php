9ec5b502586559a5bcb81fac23fa2e84cc7a1ef2
Salim Kanoun <salim.kanoun@gmail.com>
2020-01-22 23:58:55 +0100

    .gitignore fix

--- src/data/cron/config_ftp.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('FTP_HOSTNAME', 'domain.org');
-    define('FTP_USERNAME', 'login');
-    define('FTP_PASSWORD', 'password');
-    define('FTP_PORT', 22 );
-    define('FTP_IS_SFTP', true );
-    
