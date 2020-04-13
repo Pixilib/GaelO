680e6d2d8272f8be828a27916680a2530779f9a1
Salim Kanoun <salim.kanoun@gmail.com>
2020-04-13 23:36:50 +0200

    update copyright

--- src/models/Visit_Type_Iterator.php
+++ src/models/Visit_Type_Iterator.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ Copyright (C) 2018-2020 KANOUN Salim
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the Affero GNU General Public v.3 License as published by
+ the Free Software Foundation;
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ Affero GNU General Public Public for more details.
+ You should have received a copy of the Affero GNU General Public Public along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
 
 /**
  * Iterate in Visit Type order in a Visit Group
