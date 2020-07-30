9c7d525a5d6d2c2e454a1d9cd868f2b5f0e21dad
Salim Kanoun <salim.kanoun@gmail.com>
2020-07-30 15:07:35 +0200

    reverse proxy fix

--- src/scripts/tus_upload.php
+++ src/scripts/tus_upload.php
@@ -1,8 +1,9 @@
 <?php 
 
-//use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
 
 //SK reste a implementer securite
+
 require_once($_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php');
 
 define('UPLOADS_DIR', $_SERVER['DOCUMENT_ROOT'].'/data/upload/temp');
@@ -11,9 +12,9 @@ define('UPLOADS_DIR', $_SERVER['DOCUMENT_ROOT'].'/data/upload/temp');
 $server = new \TusPhp\Tus\Server(); //Either redis, file or apcu. Leave empty for file based cache.
 $server->setUploadDir(UPLOADS_DIR);
 //Overring the 'file' default root path by 'tus'
-//error_log(print_r($_SERVER, true));
 $server->setApiPath('/tus');
-//$server->getRequest()->getRequest()->setTrustedProxies(['127.0.0.1', 'REMOTE_ADDR'], Request::HEADER_X_FORWARDED_ALL);
+//Allow localhost reverse proxy
+$server->getRequest()->getRequest()->setTrustedProxies(['127.0.0.1', 'REMOTE_ADDR'], Request::HEADER_X_FORWARDED_ALL);
 
 
 $response = $server->serve();
