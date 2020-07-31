157d4480e0662ccc15d2873c2c358746f6b6582f
Emilie <emilie.olivie@hotmail.fr>
2020-07-31 09:17:20 +0200

    fix class names

--- GaelO2/GaelO/database/migrations/2020_07_30_144733_create_documentation_table.php
+++ GaelO2/GaelO/database/migrations/2020_07_30_144733_create_documentation_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Documentation extends Migration
+class CreateDocumentationTable extends Migration
 {
     /**
      * Run the migrations.
