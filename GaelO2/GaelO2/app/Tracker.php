8e3427c5d554893de948c19614017d601d856e77
Salim Kanoun <salim.kanoun@gmail.com>
2020-09-21 21:24:12 +0200

    update tracker model

--- GaelO2/GaelO2/app/Tracker.php
+++ GaelO2/GaelO2/app/Tracker.php
@@ -6,8 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tracker extends Model
 {
-    protected $primaryKey = ['date', 'user_id'];
-    public $incrementing = false;
 
     public function user(){
         return $this->belongsTo('App\User', 'user_id', 'id');
