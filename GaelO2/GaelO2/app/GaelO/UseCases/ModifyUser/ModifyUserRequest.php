a5d051bcf65e3b67efadf2a3a201d2108b99cb26
Emilie <emilie.olivie@hotmail.fr>
2020-10-14 12:09:01 +0200

    fix on modifyUser

--- GaelO2/GaelO2/app/GaelO/UseCases/ModifyUser/ModifyUserRequest.php
+++ GaelO2/GaelO2/app/GaelO/UseCases/ModifyUser/ModifyUserRequest.php
@@ -4,14 +4,14 @@ namespace App\GaelO\UseCases\ModifyUser;
 
 class ModifyUserRequest {
     public int $currentUserId;
-    public ?string $lastname = null;
-    public ?string $firstname = null;
-    public string $username ;
-    public string $email ;
+    public string $lastname;
+    public string $firstname;
+    public string $username;
+    public string $email;
     public ?string $phone = null;
     public string $status;
     public bool $administrator;
-    public int $centerCode ;
+    public int $centerCode;
     public string $job;
     public ?string $orthancAddress = null;
     public ?string $orthancLogin = null;
