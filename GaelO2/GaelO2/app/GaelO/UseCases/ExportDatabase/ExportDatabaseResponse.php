deb9fb716f390b992b2f702548407dc61e13bb14
Emilie <emilie.olivie@hotmail.fr>
2020-10-14 13:07:41 +0200

    fix

--- GaelO2/GaelO2/app/GaelO/UseCases/ExportDatabase/ExportDatabaseResponse.php
+++ GaelO2/GaelO2/app/GaelO/UseCases/ExportDatabase/ExportDatabaseResponse.php
@@ -2,7 +2,7 @@
 
 namespace App\GaelO\UseCases\ExportDatabase;
 
-class ExportDatabaseReponse {
+class ExportDatabaseResponse {
     public int $status;
     public string $statusText;
     public string $zipFile;
