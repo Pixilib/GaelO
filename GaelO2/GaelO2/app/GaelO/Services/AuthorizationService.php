47a16ae4c0dc738d0bdb206c5a6c95848da42cf2
Salim Kanoun <salim.kanoun@gmail.com>
2020-10-25 01:00:06 +0200

    first think of auth

---
+++ GaelO2/GaelO2/app/GaelO/Services/AuthorizationService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\GaelO\Services;
+
+class AuthorizationService {
+
+    //SK A DESIGNER
+    //Peut etre besoin d'un GaelOAuthorizationExecption (pour lancer un unauthorized)
+    //Appel via des statiques ?
+
+    public function isRoleAllowed(){
+
+    }
+
+    public function isPatientAllowed(){
+
+    }
+
+    public function isVisitAllowed(){
+
+    }
+
+}
