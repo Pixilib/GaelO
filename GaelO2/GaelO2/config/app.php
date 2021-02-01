d2a7fe16bb62c02d16cca59bc0e95aa4b7e0e766
EmilieOLIVIE <emilie.olivie@hotmail.fr>
2021-02-01 15:01:45 +0100

    composer.lock up

--- GaelO2/GaelO2/config/app.php
+++ GaelO2/GaelO2/config/app.php
@@ -191,6 +191,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\TelescopeProvider::class,
         App\Providers\EmailServiceProvider::class,
         App\Providers\RepositoryProvider::class
