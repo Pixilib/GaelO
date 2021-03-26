0463f810c771a9fed0c459e67c38daa9f186d1b6
EmilieOLIVIE <emilie.olivie@hotmail.fr>
2021-03-26 19:25:08 +0100

    commit

--- GaelO2/GaelO2/app/GaelO/Exceptions/GaelOForbiddenException.php
+++ GaelO2/GaelO2/app/GaelO/Exceptions/GaelOForbiddenException.php
@@ -6,7 +6,7 @@ use App\GaelO\Exceptions\GaelOException;
 
 class GaelOForbiddenException extends GaelOException {
 
-    public function __construct(){
-        parent::__construct("", 403, "Forbidden");
+    public function __construct(string $message){
+        parent::__construct($message, 403, "Forbidden");
     }
 }
