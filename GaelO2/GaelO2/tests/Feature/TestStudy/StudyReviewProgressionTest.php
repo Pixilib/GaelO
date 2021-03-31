8e862ea583852d1b06ad1e7e22db77cf0d9c66c5
Salim Kanoun <salim.kanoun@gmail.com>
2021-04-01 01:09:25 +0200

    debug test

--- GaelO2/GaelO2/tests/Feature/TestStudy/StudyReviewProgressionTest.php
+++ GaelO2/GaelO2/tests/Feature/TestStudy/StudyReviewProgressionTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\TestStudy;
 
 use App\GaelO\Constants\Constants;
 use App\Models\Review;
+use App\Models\ReviewStatus;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -31,6 +32,7 @@ class StudyReviewProgressionTest extends TestCase
     public function testGetReviewProgression() {
         $reviewerUser = User::factory()->create();
         $review = Review::factory()->reviewForm()->userId($reviewerUser->id)->validated()->create();
+        ReviewStatus::factory()->visitId($review->visit->id)->studyName($review->study_name)->create();
 
         Role::factory()->studyName($review->study_name)->userId($reviewerUser->id)->roleName(Constants::ROLE_REVIEWER)->create();
         Role::factory()->studyName($review->study_name)->roleName(Constants::ROLE_REVIEWER)->count(5)->create();
@@ -39,9 +41,9 @@ class StudyReviewProgressionTest extends TestCase
         AuthorizationTools::addRoleToUser($currentUserId, Constants::ROLE_SUPERVISOR, $review->study_name);
 
         $answer = $this->json('GET', '/api/studies/'.$review->study_name.'/visit-types/'.$review->visit->visitType->id.'/review-progression');
+
         $answer->assertStatus(200);
         $answerArray = json_decode($answer->content(), true);
-        //dd($answerArray);
         $this->assertEquals(1, sizeof($answerArray[0]['reviewDoneBy']));
         $this->assertEquals(5, sizeof($answerArray[0]['reviewNotDoneBy']));
 
