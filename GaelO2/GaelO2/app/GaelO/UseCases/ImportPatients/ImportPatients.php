335aef790c7f516b2d447e37ec2410a5af28f60d
Salim Kanoun <salim.kanoun@gmail.com>
2021-07-24 23:56:05 +0200

    small refac

--- GaelO2/GaelO2/app/GaelO/UseCases/ImportPatients/ImportPatients.php
+++ GaelO2/GaelO2/app/GaelO/UseCases/ImportPatients/ImportPatients.php
@@ -16,34 +16,36 @@ use App\GaelO\Entities\PatientEntity;
 use App\GaelO\Util;
 use Exception;
 
-class ImportPatients {
+class ImportPatients
+{
 
     private TrackerRepositoryInterface $trackerRepositoryInterface;
     private MailServices $mailService;
     private ImportPatientService $importPatient;
     private AuthorizationService $authorizationService;
 
-    public function __construct(TrackerRepositoryInterface $trackerRepositoryInterface, MailServices $mailService, ImportPatientService $importPatient, AuthorizationService $authorizationService){
+    public function __construct(TrackerRepositoryInterface $trackerRepositoryInterface, MailServices $mailService, ImportPatientService $importPatient, AuthorizationService $authorizationService)
+    {
         $this->importPatient = $importPatient;
         $this->trackerRepositoryInterface = $trackerRepositoryInterface;
         $this->mailService = $mailService;
         $this->authorizationService = $authorizationService;
-     }
+    }
 
-     public function execute(ImportPatientsRequest $importPatientsRequest, ImportPatientsResponse $importPatientsResponse) : void
+    public function execute(ImportPatientsRequest $importPatientsRequest, ImportPatientsResponse $importPatientsResponse): void
     {
-        try{
+        try {
 
             $this->checkAuthorization($importPatientsRequest->currentUserId, $importPatientsRequest->studyName);
             $arrayPatients = [];
-            foreach($importPatientsRequest->patients as $patient) {
+            foreach ($importPatientsRequest->patients as $patient) {
 
-                foreach($patient as $key => $value){
+                foreach ($patient as $key => $value) {
                     $patient[Util::camelCaseToSnakeCase($key)] = $value;
-                    $patient['study_name'] = $importPatientsRequest->studyName;
-                    $patient['withdraw_reason'] = null;
-                    $patient['withdraw_date'] = null;
                 }
+                $patient['study_name'] = $importPatientsRequest->studyName;
+                $patient['withdraw_reason'] = null;
+                $patient['withdraw_date'] = null;
                 $arrayPatients[] = PatientEntity::fillFromDBReponseArray($patient);
             }
             $importPatientsRequest->patients = $arrayPatients;
@@ -54,36 +56,31 @@ class ImportPatients {
             $this->importPatient->import();
 
             //Save action in Tracker
-            $actionDetails['Success']=$this->importPatient->successList;
-            $actionDetails['Fail']=$this->importPatient->failList;
+            $actionDetails['Success'] = $this->importPatient->successList;
+            $actionDetails['Fail'] = $this->importPatient->failList;
 
-            $importPatientsResponse->body = [ 'success' => $this->importPatient->successList, 'fail' => $this->importPatient->failList];
+            $importPatientsResponse->body = ['success' => $this->importPatient->successList, 'fail' => $this->importPatient->failList];
             $importPatientsResponse->status = 200;
             $importPatientsResponse->statusText = 'OK';
 
             $this->trackerRepositoryInterface->writeAction($importPatientsRequest->currentUserId, Constants::TRACKER_IMPORT_PATIENT, $importPatientsRequest->studyName, null, Constants::TRACKER_IMPORT_PATIENT, $actionDetails);
 
             $this->mailService->sendImportPatientMessage($importPatientsRequest->studyName, $this->importPatient->successList, $this->importPatient->failList);
-
-        } catch (GaelOException $e){
+        } catch (GaelOException $e) {
 
             $importPatientsResponse->body = $e->getErrorBody();
             $importPatientsResponse->status = $e->statusCode;
             $importPatientsResponse->statusText = $e->statusText;
-
-        } catch (Exception $e){
+        } catch (Exception $e) {
             throw $e;
         }
-
-
     }
 
-    private function checkAuthorization(int $userId, string $studyName){
+    private function checkAuthorization(int $userId, string $studyName)
+    {
         $this->authorizationService->setCurrentUserAndRole($userId, Constants::ROLE_SUPERVISOR);
-        if(! $this->authorizationService->isRoleAllowed($studyName)){
+        if (!$this->authorizationService->isRoleAllowed($studyName)) {
             throw new GaelOForbiddenException();
         };
-
     }
-
 }
