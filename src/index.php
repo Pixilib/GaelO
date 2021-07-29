0549fdf187f234ffc9c598656009272160f04964
Salim Kanoun <salim.kanoun@gmail.com>
2021-07-30 00:43:33 +0200

    1.5.4

--- src/index.php
+++ src/index.php
@@ -19,7 +19,7 @@
  * @package     GaelO
  * @author      Salim Kanoun <Salim.Kanoun@gmail.com>
  */
-define('GAELO_VERSION', '1.5.3');
+define('GAELO_VERSION', '1.5.4');
 header('content-type: text/html; charset=utf-8');
 require_once('vendor/autoload.php');
 require 'router.php';
