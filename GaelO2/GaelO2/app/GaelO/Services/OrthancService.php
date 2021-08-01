21758b3e79ea7dc60fdd6ede83be2d4293f6112d
Salim Kanoun <salim.kanoun@gmail.com>
2021-08-01 15:14:52 +0200

    update anonymisation rules

--- GaelO2/GaelO2/app/GaelO/Services/OrthancService.php
+++ GaelO2/GaelO2/app/GaelO/Services/OrthancService.php
@@ -20,7 +20,7 @@ class OrthancService
         $this->frameworkInterface = $frameworkInterface;
     }
 
-    public function setOrthancServer(bool $storage) : void
+    public function setOrthancServer(bool $storage): void
     {
         //Set Time Limit at 3H as operation could be really long
         set_time_limit(10800);
@@ -42,19 +42,22 @@ class OrthancService
         $this->httpClientInterface->setBasicAuthentication($login, $password);
     }
 
-    public function getOrthancRessourcesDetails(string $level, string $orthancID) : array {
-        return $this->httpClientInterface->requestJson('GET', '/'.$level.'/'.$orthancID)->getJsonBody();
+    public function getOrthancRessourcesDetails(string $level, string $orthancID): array
+    {
+        return $this->httpClientInterface->requestJson('GET', '/' . $level . '/' . $orthancID)->getJsonBody();
     }
 
-    public function getOrthancRessourcesStatistics(string $level, string $orthancID) : array {
-        return $this->httpClientInterface->requestJson('GET', '/'.$level.'/'.$orthancID.'/statistics/')->getJsonBody();
+    public function getOrthancRessourcesStatistics(string $level, string $orthancID): array
+    {
+        return $this->httpClientInterface->requestJson('GET', '/' . $level . '/' . $orthancID . '/statistics/')->getJsonBody();
     }
 
-    public function getInstanceTags(string $orthancInstanceID) : array {
-        return $this->httpClientInterface->requestJson('GET', '/instances/'.$orthancInstanceID.'/tags/')->getJsonBody();
+    public function getInstanceTags(string $orthancInstanceID): array
+    {
+        return $this->httpClientInterface->requestJson('GET', '/instances/' . $orthancInstanceID . '/tags/')->getJsonBody();
     }
 
-    public function getOrthancPeers() : array
+    public function getOrthancPeers(): array
     {
         return $this->httpClientInterface->requestJson('GET', '/peers')->getJsonBody();
     }
@@ -83,7 +86,7 @@ class OrthancService
     /**
      * Remove all peers from orthanc
      */
-    public function removeAllPeers() : void
+    public function removeAllPeers(): void
     {
         $peers = $this->getOrthancPeers();
 
@@ -101,7 +104,7 @@ class OrthancService
         string $studyUID = '',
         string $accessionNumber = '',
         string $studyDescription = ''
-    ) : array {
+    ): array {
 
         $query = array(
             'Level' => $level,
@@ -126,7 +129,7 @@ class OrthancService
         $this->httpClientInterface->rowRequest('DELETE', '/' . $level . '/' . $uid, null, null);
     }
 
-    public function isPeerAccelerated(string $peer) : bool
+    public function isPeerAccelerated(string $peer): bool
     {
 
         $peers = $this->httpClientInterface->rowRequest('GET', '/transfers/peers/', null, null)->getJsonBody();
@@ -173,10 +176,10 @@ class OrthancService
         return $this->httpClientInterface->requestJson('POST', '/transfers/send', $data);
     }
 
-    public function importFiles(array $files) : array
+    public function importFiles(array $files): array
     {
         $psr7ResponseAdapterArray = $this->httpClientInterface->requestUploadArrayDicom('POST', '/instances', $files);
-        $arrayAnswer = array_map( function ($response){
+        $arrayAnswer = array_map(function ($response) {
             return json_decode($response->getBody(), true);
         }, $psr7ResponseAdapterArray);
         return $arrayAnswer;
@@ -194,7 +197,7 @@ class OrthancService
      * @param string $studyName
      * @return string anonymizedOrthancStudyID
      */
-    public function anonymize(string $studyID, string $profile, string $patientCode, string $visitType, string $studyName) : string
+    public function anonymize(string $studyID, string $profile, string $patientCode, string $visitType, string $studyName): string
     {
 
         $jsonAnonQuery = $this->buildAnonQuery($profile, $patientCode, $patientCode, $visitType, $studyName);
@@ -225,26 +228,16 @@ class OrthancService
         string $newPatientID,
         string $newStudyDescription,
         string $clinicalStudy
-    ) : array {
+    ): array {
 
         $tagsObjects = [];
 
-        if ($profile == Constants::ORTHANC_ANON_PROFILE_DEFAULT) {
+        if ($profile === Constants::ORTHANC_ANON_PROFILE_DEFAULT) {
             $date = TagAnon::KEEP;
             $body = TagAnon::KEEP;
-
-            $tagsObjects[] = new TagAnon("0010,0030", TagAnon::REPLACE, "19000101"); // BirthDay
-            $tagsObjects[] = new TagAnon("0008,1030", TagAnon::REPLACE, $newStudyDescription); //studyDescription
-            $tagsObjects[] = new TagAnon("0008,103E", TagAnon::KEEP); //series Description
-
-
-        } else if ($profile == Constants::ORTHANC_ANON_PROFILE_FULL) {
+        } else if ($profile === Constants::ORTHANC_ANON_PROFILE_FULL) {
             $date = TagAnon::CLEAR;
             $body = TagAnon::CLEAR;
-
-            $tagsObjects[] = new TagAnon("0010,0030", TagAnon::REPLACE, "19000101"); // BirthDay
-            $tagsObjects[] = new TagAnon("0008,1030", TagAnon::CLEAR); // studyDescription
-            $tagsObjects[] = new TagAnon("0008,103E", TagAnon::CLEAR); //series Description
         }
 
         //List tags releted to Date
@@ -259,13 +252,12 @@ class OrthancService
         $tagsObjects[] = new TagAnon("0008,0033", $date); // Content Time
         $tagsObjects[] = new TagAnon("0008,0024", $date); // Overlay Date
         $tagsObjects[] = new TagAnon("0008,0034", $date); // Overlay Time
-        $tagsObjects[] = new TagAnon("0040,0244", $date); // ...Start Date
-        $tagsObjects[] = new TagAnon("0040,0245", $date); // ...Start Time
+        $tagsObjects[] = new TagAnon("0040,0244", $date); // Performed Procedure Step Start Date
+        $tagsObjects[] = new TagAnon("0040,0245", $date); // Performed Procedure Step Start Start Time
         $tagsObjects[] = new TagAnon("0008,0021", $date); // Series Date
         $tagsObjects[] = new TagAnon("0008,0031", $date); // Series Time
         $tagsObjects[] = new TagAnon("0008,0020", $date); // Study Date
         $tagsObjects[] = new TagAnon("0008,0030", $date); // Study Time
-        $tagsObjects[] = new TagAnon("0010,21D0", $date); // Last menstrual date
         $tagsObjects[] = new TagAnon("0008,0201", $date); // Timezone offset from UTC
         $tagsObjects[] = new TagAnon("0040,0002", $date); // Scheduled procedure step start date
         $tagsObjects[] = new TagAnon("0040,0003", $date); // Scheduled procedure step start time
@@ -273,12 +265,7 @@ class OrthancService
         $tagsObjects[] = new TagAnon("0040,0005", $date); // Scheduled procedure step end time
 
         // same for Body characteristics
-        $tagsObjects[] = new TagAnon("0010,2160", $body); // Patient's ethnic group
-        $tagsObjects[] = new TagAnon("0010,21A0", $body); // Patient's smoking status
         $tagsObjects[] = new TagAnon("0010,0040", $body); // Patient's sex
-        $tagsObjects[] = new TagAnon("0010,2203", $body); // Patient's sex neutered
-        $tagsObjects[] = new TagAnon("0010,1010", $body); // Patient's age
-        $tagsObjects[] = new TagAnon("0010,21C0", $body); // Patient's pregnancy status
         $tagsObjects[] = new TagAnon("0010,1020", $body); // Patient's size
         $tagsObjects[] = new TagAnon("0010,1030", $body); // Patient's weight
 
@@ -286,6 +273,11 @@ class OrthancService
         $tagsObjects[] = new TagAnon("0008,0050", TagAnon::REPLACE, $clinicalStudy); // Accession Number contains study name
         $tagsObjects[] = new TagAnon("0010,0020", TagAnon::REPLACE, $newPatientID); //new Patient Name
         $tagsObjects[] = new TagAnon("0010,0010", TagAnon::REPLACE, $newPatientName); //new Patient Name
+        $tagsObjects[] = new TagAnon("0008,1030", TagAnon::REPLACE, $newStudyDescription); //studyDescription
+        $tagsObjects[] = new TagAnon("0010,0030", TagAnon::REPLACE, "19000101"); // BirthDay
+
+        //Keep series descriptions
+        $tagsObjects[] = new TagAnon("0008,103E", TagAnon::KEEP); //series Description
 
         // Keep some Private tags usefull for PET/CT or Scintigraphy
         $tagsObjects[] = new TagAnon("7053,1000", TagAnon::KEEP); //Phillips
@@ -335,20 +327,22 @@ class OrthancService
      * @param String $jobId
      * @return mixed
      */
-    public function getJobDetails(String $jobId) {
-        return $this->httpClientInterface->requestJson('GET', '/jobs/'.$jobId)->getJsonBody();
+    public function getJobDetails(String $jobId)
+    {
+        return $this->httpClientInterface->requestJson('GET', '/jobs/' . $jobId)->getJsonBody();
     }
 
-    public function getStudyOrthancDetails(string $orthancStudyID){
+    public function getStudyOrthancDetails(string $orthancStudyID)
+    {
         $studyOrthanc = new OrthancStudy($this);
         $studyOrthanc->setStudyOrthancID($orthancStudyID);
         $studyOrthanc->retrieveStudyData();
         return $studyOrthanc;
     }
 
-    public function getOrthancZipStream(array $seriesOrthancIDs){
-        $payload = array('Transcode'=>'1.2.840.10008.1.2.1', 'Resources' => $seriesOrthancIDs);
+    public function getOrthancZipStream(array $seriesOrthancIDs)
+    {
+        $payload = array('Transcode' => '1.2.840.10008.1.2.1', 'Resources' => $seriesOrthancIDs);
         $this->httpClientInterface->streamResponse('POST', '/tools/create-archive', $payload);
     }
-
 }
