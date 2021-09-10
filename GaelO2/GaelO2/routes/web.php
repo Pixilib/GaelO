f1a71d60e159c7ddceaa5196ee0e8f3060fe820c
Salim Kanoun <salim.kanoun@gmail.com>
2021-09-10 23:01:30 +0200

    testing

--- GaelO2/GaelO2/routes/web.php
+++ GaelO2/GaelO2/routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'Welcome to GaelO';
+    Log::info(request()->ip());
+    return getallheaders();
 });
 
