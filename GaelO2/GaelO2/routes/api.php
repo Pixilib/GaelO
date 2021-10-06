7fa4ef1abb9dfcad8ccf2dc61e33779a3ddb3612
EmilieOLIVIE <emilie.olivie@hotmail.fr>
2021-10-06 08:46:12 +0200

    GetStudyDetails - added visitType context

--- GaelO2/GaelO2/routes/api.php
+++ GaelO2/GaelO2/routes/api.php
@@ -167,7 +167,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
     //Tools routes
     Route::post('tools/centers/patients-from-centers', [ToolsController::class, 'getPatientsInStudyFromCenters']);
     Route::post('tools/patients/visits-from-patients', [ToolsController::class, 'getPatientsVisitsInStudy']);
-    Route::post('tools/visit-types/visit-types-details', [ToolsController::class, 'getVisitTypesDetails']);
 
     // Binary routes
     Route::get('export-db', [ExportDBController::class, 'exportDB']);
