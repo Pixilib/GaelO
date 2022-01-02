fcdc01fc4dc70973581042ad71bc4fbd48630653
Salim Kanoun <salim.kanoun@gmail.com>
2022-01-02 19:20:30 +0100

    fix invisible link in verify email

--- GaelO2/GaelO2/app/GaelO/views/mails/mail_verify_email.blade.php
+++ GaelO2/GaelO2/app/GaelO/views/mails/mail_verify_email.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div>
-        Please follow this link to verify your email <a href={{$url}}><br>
+        Please follow <a href={{$url}}>this link to verify your email<br>
     </div>
     <div>
         For further informations read our <a href="https://www.gaelo.fr/documentation">documentation</a><br>
