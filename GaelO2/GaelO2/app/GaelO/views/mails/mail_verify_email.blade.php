2801ab98c95b7e7208ea6c1ddb21f3d0449a1784
Salim Kanoun <salim.kanoun@gmail.com>
2022-01-09 00:34:07 +0100

    small

--- GaelO2/GaelO2/app/GaelO/views/mails/mail_verify_email.blade.php
+++ GaelO2/GaelO2/app/GaelO/views/mails/mail_verify_email.blade.php
@@ -4,8 +4,5 @@
     <div>
         Please follow <a href={{$url}}>this link to verify your email<br>
     </div>
-    <div>
-        For further informations read our <a href="https://www.gaelo.fr/documentation">documentation</a><br>
-    </div>
 
 @endsection
