147edd5c78f3c3b133615040654ad42cf1023d9c
Salim Kanoun <salim.kanoun@gmail.com>
2022-01-24 23:58:28 +0100

    typo recieved

--- GaelO2/GaelO2/app/GaelO/views/mails/mail_reset_password.blade.php
+++ GaelO2/GaelO2/app/GaelO/views/mails/mail_reset_password.blade.php
@@ -1,7 +1,7 @@
 @extends('mails.mail_template')
 
 @section('content')
-    A password reset request has been recieved for this account<br>
+    A password reset request has been received for this account<br>
     Use <a href={{$url}}> this link to reset your password</a>  (24h validity)<br>
     If you didn't asked for a password reset, please contact the administrator<br>
     {{$adminEmail}}<br>
