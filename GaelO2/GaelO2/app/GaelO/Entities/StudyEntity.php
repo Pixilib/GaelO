a27c6a569eefaa76e28b4c4b3b603d3ae84f4a6d
Salim Kanoun <salim.kanoun@gmail.com>
2022-02-26 00:22:49 +0100

    expose contact email on study entity

--- GaelO2/GaelO2/app/GaelO/Entities/StudyEntity.php
+++ GaelO2/GaelO2/app/GaelO/Entities/StudyEntity.php
@@ -6,6 +6,7 @@ class StudyEntity {
     public string $name;
     public string $code;
     public int $patientCodeLength;
+    public string $contactEmail;
     public bool $deleted;
     public ?string $ancillaryOf;
 
@@ -14,6 +15,7 @@ class StudyEntity {
         $studyEntity->name = $array['name'];
         $studyEntity->code = $array['code'];
         $studyEntity->patientCodeLength = $array['patient_code_length'];
+        $studyEntity->contactEmail = $array['contact_email'];
         $studyEntity->ancillaryOf = $array['ancillary_of'];
         $studyEntity->deleted = $array['deleted_at'] !== null;
 
