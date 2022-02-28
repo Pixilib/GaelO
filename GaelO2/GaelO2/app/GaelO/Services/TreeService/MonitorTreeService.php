ebaab388720f0bc83febee33dfdcb003982e06cf
Salim Kanoun <salim.kanoun@gmail.com>
2022-02-28 23:17:26 +0100

    monitor permission scoped by user center by default

--- GaelO2/GaelO2/app/GaelO/Services/TreeService/MonitorTreeService.php
+++ GaelO2/GaelO2/app/GaelO/Services/TreeService/MonitorTreeService.php
@@ -4,14 +4,8 @@ namespace App\GaelO\Services\TreeService;
 
 use App\GaelO\Constants\Constants;
 
-class MonitorTreeService extends AbstractTreeService{
+class MonitorTreeService extends InvestigatorTreeService{
 
     protected string $role = Constants::ROLE_MONITOR;
-
-    public function buildTree(): array
-    {
-        $visitsArray = $this->visitRepository->getVisitsInStudy($this->studyName, false, false);
-        return  $this->makeTreeFromVisits($visitsArray);
-    }
 
 }
