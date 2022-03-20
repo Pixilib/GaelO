4f3336f8be3049d7b3388696162c708024b09f93
Salim Kanoun <salim.kanoun@gmail.com>
2022-03-20 22:22:32 +0100

    small

--- src/data/cron/cron.php
+++ src/data/cron/cron.php
@@ -1,6 +1,6 @@
 <?php
 /**
- Copyright (C) 2018-2020 KANOUN Salim
+ Copyright (C) 2018 KANOUN Salim
  This program is free software; you can redistribute it and/or modify
  it under the terms of the Affero GNU General Public v.3 License as published by
  the Free Software Foundation;
@@ -24,15 +24,34 @@ require_once(__DIR__.'/../../vendor/autoload.php');
 use GO\Scheduler;
 
 // Create a new scheduler
-$scheduler=new Scheduler();
+$scheduler = new Scheduler();
 
 //Define action and timing
 
 
-//scheduleEveryXMinutes("import_from_email_ennov.php", ["study" =>"TRAILOCLORI"] , 1);
+//TO BE STARTED EMAIL MONITORING
+//scheduleEveryXMinutes("monitor_ennov_email.php", [] , 1);
+
+
+/* Disabled DESCART monitoring until new instruction
+scheduleWorkingDays("import_patient_DESCART.php", ["study" =>"DESCART","limitUpdate" => (24*60)] , 9, 0);
+scheduleWorkingDays("import_patient_DESCART.php", ["study" =>"DESCART","limitUpdate" => (24*60)] , 14, 0);
+
+scheduleWorkingDays("import_file_from_ftp.php", ["study" => "DESCART",
+                                                "LimitUpdate" => (24*60), 
+                                                "path" => "/data/upload/from_ftp/DESCART",
+                                                "filename" =>"DESCART_PATIENTS.txt"]
+                                            , 9, 01);
+
+scheduleWorkingDays("import_file_from_ftp.php", ["study" => "DESCART",
+                                                "LimitUpdate" => (24*60), 
+                                                "path" => "/data/upload/from_ftp/DESCART",
+                                                "filename" =>"DESCART_PATIENTS.txt"]
+                                            , 14, 01);
+*/
+
 // Let the scheduler execute jobs which are due.
 $scheduler->run();
-
 
 function testScheduleForDebug(String $scriptName, array $arugments, int $hour, int $min){
     global $scheduler;
@@ -56,6 +75,11 @@ function scheduleEveryDays(String $scriptName, array $arugments, int $hour, int 
 function scheduleSundays(String $scriptName, array $arugments, int $hour, int $min){
     global $scheduler;
     $scheduler->php(__DIR__.'/'.$scriptName, null, $arugments)->sunday($hour, $min)->output('/var/log/gaelo_cron_'.$scriptName.'.log');
+}
+
+function scheduleMondays(String $scriptName, array $arugments, int $hour, int $min){
+    global $scheduler;
+    $scheduler->php(__DIR__.'/'.$scriptName, null, $arugments)->monday($hour, $min)->output('/var/log/gaelo_cron_'.$scriptName.'.log');
 }
 
 function scheduleEveryXMinutes(String $scriptName, array $arugments, int $min){
