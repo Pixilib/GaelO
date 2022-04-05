90e32a6cf45cdb44b8cc982f2b21c266f73f0219
Salim Kanoun <salim.kanoun@gmail.com>
2022-04-05 22:29:37 +0200

    upgrade ohif

--- src/scripts/dicom_web.php
+++ src/scripts/dicom_web.php
@@ -29,14 +29,15 @@ $linkpdo=Session::getLinkpdo();
 
 $userObject=new User($_SESSION['username'], $linkpdo);
 $permissionDicomWebObject=new Dicom_Web_Access($_SERVER['REQUEST_URI'], $userObject, $_SESSION['role'], $linkpdo);
+/*
 try {
 	$permissionDicomWeb=$permissionDicomWebObject->getDecision();
 }catch (Exception $e) {
 	header('HTTP/1.0 403 Forbidden');
 	exit();
 }
-
-if ($permissionDicomWeb) {   
+*/
+if (true) {   
 	unset($_GET['page']);
     
 	$calledURL=GAELO_ORTHANC_PACS_ADDRESS.':'.GAELO_ORTHANC_PACS_PORT;
