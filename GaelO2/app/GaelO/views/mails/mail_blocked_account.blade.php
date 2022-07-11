8117d371739ec6a56a5f88c66f866bf5db08bc81
salim <salim.kanoun@gmail.com>
2022-07-11 21:31:57 +0200

    fix blocked account email

--- GaelO2/app/GaelO/views/mails/mail_blocked_account.blade.php
+++ GaelO2/app/GaelO/views/mails/mail_blocked_account.blade.php
@@ -1,12 +1,12 @@
 @extends('mails.mail_template')
 
 @section('content')
-    The following user account is blocked after too many bad password attempts.<br>
-    Email : {{$email}}<br>
-    The account is linked to the following studies:<br>
+The following user account is blocked after too many bad password attempts.<br>
+Email : {{$email}}<br>
+The account is linked to the following studies:
+<ul>
     @foreach ($studies as $study)
-        <li>{{$study}}</li>
+    <li>{{$study}}</li>
     @endforeach
+</ul>
 @endsection
-
-
