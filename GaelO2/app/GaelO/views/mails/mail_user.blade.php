c3974aa6991fce9ce28da386ec476246adc02f77
salim <salim.kanoun@gmail.com>
2022-08-12 09:46:32 +0200

    i to em

--- GaelO2/app/GaelO/views/mails/mail_user.blade.php
+++ GaelO2/app/GaelO/views/mails/mail_user.blade.php
@@ -3,26 +3,26 @@
 @section('content')
     {!!$content!!}
     @if(!empty($patientId) && empty($visitId) && !empty($study))
-        <i>This e-mail is related to
+        <em>This e-mail is related to
             <a href="{{$webAddress}}/study/{{$study}}/role/Supervisor/patient/{{$patientId}}">
                 patient {{$patientId}}
             </a>
-        </i>
+        </em>
     @endif
     @if(!empty($patientId) && !empty($visitId) && !empty($study))
-        <i>This e-mail is related to
+        <em>This e-mail is related to
             <a href="{{$webAddress}}/study/{{$study}}/role/Supervisor/visit/{{$visitId}}">
                 visit {{$visitId}}
                 of patient {{$patientId}}
             </a>
-        </i>
+        </em>
     @endif
     @if(!empty($patients) && !empty($study))
-        <i>
+        <em>
             <a href="{{$webAddress}}/study/{{$study}}/role/Supervisor/patients">
                 Connect to the platform
             </a>
             if you want to add these patients to {{$study}} study
-        </i>
+        </em>
     @endif
 @endsection
