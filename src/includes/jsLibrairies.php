cbd06043e2727bca2b78c8018a5c39ed1b680686
Salim Kanoun <salim.kanoun@gmail.com>
2022-08-28 14:47:18 +0200

    fix cross origin

--- src/includes/jsLibrairies.php
+++ src/includes/jsLibrairies.php
@@ -19,29 +19,29 @@
 
 <!-- JavaScript  JQuery-->
 <script
-	src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" crossorigin ></script>
 
 <!-- JavaScript  BootStrap -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" crossorigin >
 	
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" crossorigin ></script>
 
 	
 <!-- JavaScript  JQueryUI -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" crossorigin />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js" crossorigin ></script>
 
 <!-- Javascript Bootstrap DatePicker-->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css" crossorigin />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js" crossorigin ></script>
 
 <!-- JavaScript  Alertify -->
 <link rel="stylesheet"
-	href="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.11.4/css/alertify.min.css" />
+	href="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.11.4/css/alertify.min.css" crossorigin />
 <link rel="stylesheet"
-	href="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.11.4/css/themes/default.min.css" />
+	href="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.11.4/css/themes/default.min.css" crossorigin />
 <script
-	src="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.11.4/alertify.min.js" defer></script>
+	src="https://cdnjs.cloudflare.com/ajax/libs/AlertifyJS/1.11.4/alertify.min.js" defer crossorigin ></script>
 
 <script type="text/javascript" src="assets/javascripts/gaelO.js" ></script>
 <link rel="stylesheet" href="assets/css/gaelO-specific.css">
@@ -51,80 +51,80 @@
 
 
 <!-- JavaScript  Popper JS for bootstrap dropdown button -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" defer></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" defer crossorigin></script>
 
 <!-- JavaScript  JSTree -->
 <link rel="preload" as="style"
-	href="//cdnjs.cloudflare.com/ajax/libs/jstree/3.3.8/themes/default/style.min.css" />
+	href="//cdnjs.cloudflare.com/ajax/libs/jstree/3.3.8/themes/default/style.min.css" crossorigin />
 <script
-	src="//cdnjs.cloudflare.com/ajax/libs/jstree/3.3.8/jstree.min.js" defer></script>
+	src="//cdnjs.cloudflare.com/ajax/libs/jstree/3.3.8/jstree.min.js" defer crossorigin ></script>
 	
 <!-- JavaScript  Moment -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js" defer></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js" defer crossorigin></script>
 
 <!-- JavaScript  Datatable -->
 <link rel="preload" as="style" type="text/css"
-	href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
+	href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" crossorigin>
 <script type="text/javascript" charset="utf8"
-	src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js" defer></script>
+	src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js" defer crossorigin></script>
 <link rel="preload" as="style" type="text/css"
-	href="https://cdn.datatables.net/select/1.2.7/css/select.dataTables.min.css" >
+	href="https://cdn.datatables.net/select/1.2.7/css/select.dataTables.min.css" crossorigin >
 <script type="text/javascript" charset="utf8"
-	src="https://cdn.datatables.net/select/1.2.7/js/dataTables.select.min.js" defer></script>
+	src="https://cdn.datatables.net/select/1.2.7/js/dataTables.select.min.js" defer crossorigin></script>
 
 <link rel="preload" as="style" type="text/css"
-	href="https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css" >
+	href="https://cdn.datatables.net/buttons/1.5.2/css/buttons.dataTables.min.css" crossorigin >
 <script type="text/javascript" charset="utf8"
-	src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js" defer></script>
+	src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js" defer crossorigin></script>
 
 <link rel="preload" as="style" type="text/css"
-	href="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.16/b-1.5.1/b-html5-1.5.1/b-print-1.5.1/r-2.2.1/datatables.min.css" />
+	href="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.16/b-1.5.1/b-html5-1.5.1/b-print-1.5.1/r-2.2.1/datatables.min.css" crossorigin />
 <script type="text/javascript"
-	src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js" defer></script>
+	src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/pdfmake.min.js" defer crossorigin ></script>
 <script type="text/javascript"
-	src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js" defer></script>
+	src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.32/vfs_fonts.js" defer crossorigin></script>
 <script type="text/javascript"
-	src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.16/b-1.5.1/b-html5-1.5.1/b-print-1.5.1/r-2.2.1/datatables.min.js" defer></script>
+	src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.16/b-1.5.1/b-html5-1.5.1/b-print-1.5.1/r-2.2.1/datatables.min.js" defer crossorigin></script>
 
 <!-- JavaScript  Chosen -->
 <link rel="preload" as="style" type="text/css"
-	href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.min.css" >
+	href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.min.css" crossorigin >
 <script type="text/javascript"
-	src="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.jquery.min.js" defer></script>
+	src="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.jquery.min.js" defer crossorigin ></script>
 
 <!-- JavaScript  ChartJS -->
 <script
-	src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" defer></script>
+	src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" defer crossorigin ></script>
 <script
-	src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.6.0" defer></script>
+	src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@0.6.0" defer crossorigin ></script>
 
-<script src="https://cdn.jsdelivr.net/npm/chartjs-chart-box-and-violin-plot@2.1.0/build/Chart.BoxPlot.min.js" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/chartjs-chart-box-and-violin-plot@2.1.0/build/Chart.BoxPlot.min.js" defer crossorigin></script>
 	
 <!-- JavaScript  TinyMCE -->
 <script type="text/javascript"
-	src='https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.9.2/tinymce.min.js' defer></script>
+	src='https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.9.2/tinymce.min.js' defer crossorigin></script>
 
 <!-- JavaScript  jszip -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.2.0/jszip.min.js" defer></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.2.0/jszip.min.js" defer crossorigin></script>
 
 <!-- JavaScript  alaSQL -->
 <script type="text/javascript"
-	src="https://cdnjs.cloudflare.com/ajax/libs/alasql/0.4.11/alasql.min.js" defer></script>
+	src="https://cdnjs.cloudflare.com/ajax/libs/alasql/0.4.11/alasql.min.js" defer crossorigin ></script>
 <!-- JavaScript  Simple Statistics -->
 <script type="text/javascript"
-	src='https://unpkg.com/simple-statistics@7.0.2/dist/simple-statistics.min.js' defer></script>
+	src='https://unpkg.com/simple-statistics@7.0.2/dist/simple-statistics.min.js' defer crossorigin ></script>
 
 <!-- Uppy -->
-<link href="https://transloadit.edgly.net/releases/uppy/v1.12.1/uppy.min.css" rel="stylesheet">
-<script src="https://transloadit.edgly.net/releases/uppy/v1.12.1/uppy.min.js"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/uppy/1.12.1/uppy.min.css" rel="stylesheet" crossorigin>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/uppy/1.12.1/uppy.min.js" crossorigin ></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.1.0/papaparse.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.1.0/papaparse.min.js" crossorigin ></script>
 
 <!-- GaelO Uploader -->
-<script src="https://unpkg.com/gaelo-uploader@3.4.5/dist/bundle.js"></script>
-<link rel="stylesheet" href="https://unpkg.com/gaelo-uploader@3.4.5/dist/main.css">
+<script src="https://unpkg.com/gaelo-uploader@3.4.5/dist/bundle.js" crossorigin ></script>
+<link rel="stylesheet" href="https://unpkg.com/gaelo-uploader@3.4.5/dist/main.css" crossorigin >
 
-<script type="text/javascript" src="assets/javascripts/gaelO.js" ></script>
+<script type="text/javascript" src="assets/javascripts/gaelO.js"></script>
 <link rel="stylesheet" href="assets/css/gaelO-specific.css">
 <link rel="stylesheet" href="assets/css/gaelO-static.css">
 <link rel="stylesheet" href="assets/css/gaelO-fonts.css">
