94d3dfa5b79b7ee23d8c20e9026ff3e028bdd681
Théophilus Homawoo <83962803+theophilushmwo@users.noreply.github.com>
2022-09-09 16:18:58 +0200

    mockery interface

--- GaelO2/tests/Unit/TestJobs/JobAutoQcTest.php
+++ GaelO2/tests/Unit/TestJobs/JobAutoQcTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\TestJobs;
 
 use App\GaelO\Constants\Constants;
+use App\GaelO\Services\MailServices;
 use App\GaelO\Services\OrthancService;
 use App\Jobs\JobAutoQc;
 use App\Jobs\JobGaelOProcessing;
@@ -18,7 +19,12 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Bus;
 use Tests\TestCase;
+use App\GaelO\Services\StoreObjects\SharedTags;
 use Mockery;
+use App\GaelO\Interfaces\Adapters\FrameworkInterface;
+use App\GaelO\Interfaces\Repositories\DicomStudyRepositoryInterface;
+use App\GaelO\Interfaces\Repositories\UserRepositoryInterface;
+use App\GaelO\Interfaces\Repositories\VisitRepositoryInterface;
 
 class JobAutoQcTest extends TestCase
 {
@@ -34,27 +40,60 @@ class JobAutoQcTest extends TestCase
         $this->artisan('db:seed');
     }
 
-    public function testSendAutoQc(){
+    public function testSendAutoQc() {
+        $mailServices = Mockery::mock(MailServices::class);
+        $frameworkInterface = Mockery::mock(FrameworkInterface::class);
+        $visitRepositoryInterface = Mockery::mock(VisitRepositoryInterface::class);
+        $dicomStudyRepositoryInterface = Mockery::mock(DicomStudyRepositoryInterface::class);
+        $userRepositoryInterface = Mockery::mock(UserRepositoryInterface::class);
+    
         $visit = Visit::factory()->create();
         $dicomStudy = DicomStudy::factory()->visitId($visit->id)->create();
         $dicomSeries = DicomSeries::factory()->studyInstanceUID($dicomStudy->study_uid)->count(5)->create();
-
         $studyName = $visit->patient->study_name;
-
         $user = User::factory()->create();
         Role::factory()->userId($user->id)->studyName($studyName)->roleName(Constants::ROLE_CONTROLLER)->create();
-
         $strJsonFileContents = file_get_contents(getcwd()."/tests/Unit/TestJobs/sharedTags.json");
         $sharedTags = json_decode($strJsonFileContents, true);
         $mockOrthancService = Mockery::mock(OrthancService::class);
         $mockOrthancService->shouldReceive('getSharedTags')
             ->andReturn($sharedTags);
-        
         $mockOrthancService->ShouldReceive('getMIP')
             ->andReturn((getcwd()."/tests/Unit/TestJobs/test.gif"));
-        
         app()->instance(OrthancService::class, $mockOrthancService);
-        JobAutoQc::dispatchSync($visit->id);
+
+        $studyInfo = [];
+        $studyInfo['studyDescription'] = 'studyDescription';
+        $studyInfo['studyManufacturer'] = 'studyManufacturer';
+        $studyInfo['studyDate'] = 'studyDate';
+        $studyInfo['studyTime'] = 'studyTime';
+        $studyInfo['numberOfSeries'] = count($dicomSeries);
+        $studyInfo['numberOfInstances'] = 0;
+        $seriesInfo = [];
+        foreach ($dicomSeries as $series) {
+            $studyInfo['numberOfInstances'] += $series['number_of_instances'];
+            $seriesData = [];
+            $seriesData['infos'] = [];
+            $seriesData['image_path'] = (getcwd()."/tests/Unit/TestJobs/test.gif");
+            $seriesData['series_description'] = 'seriesDescription';
+            $seriesData['infos']['modality'] = 'modality';
+            $seriesData['infos']['series_date'] =  'seriesDate';
+            $seriesData['infos']['series_time'] = 'seriesTime';
+            $seriesData['infos']['slice_thickness'] = 'sliceThickness';
+            $seriesData['infos']['pixel_spacing'] = 'pixelSpacing';
+            $seriesData['infos']['fov'] = 'fov';
+            $seriesData['infos']['matrix_size'] = 'matrixSize';
+            $seriesData['infos']['patient_position'] = 'patientPosition';
+            $seriesData['infos']['patient_orientation'] = 'patientOrientation';
+            $seriesData['infos']['number_of_instances'] = 'numberOfInstances';
+            $seriesInfo[] = $seriesData;
+        }
+        $studyName = 'studyName';
+        $visitId = $visit->id;
+        $visitType = 'visitType';
+        $patientCode = 'patientCode';
+        $redirectLink = 'redirectLink';
+        $magicLink = 'magicLink';
     }
 }
 
