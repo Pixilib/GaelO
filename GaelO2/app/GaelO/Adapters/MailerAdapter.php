2aeb97de22667dbf131de5c38a7ad3571f4f5d0e
Salim Kanoun <salim.kanoun@gmail.com>
2022-10-18 12:00:36 +0200

    Update MailerAdapter.php

--- GaelO2/app/GaelO/Adapters/MailerAdapter.php
+++ GaelO2/app/GaelO/Adapters/MailerAdapter.php
@@ -38,18 +38,19 @@ class MailerAdapter implements MailerInterface
 {
 
     private FrameworkInterface $frameworkInterface;
+    private array $to;
+    private string $replyTo;
 
     public function __construct(FrameworkInterface $frameworkInterface)
     {
         $this->frameworkInterface = $frameworkInterface;
+        $this->replyTo = $this->frameworkInterface::getConfig('mail_reply_to_default');
     }
-
-    private array $to;
+   
 
     public function setReplyTo(?String $replyTo = null)
     {
-        if ($replyTo == null) $this->replyTo = $this->frameworkInterface::getConfig('mail_reply_to_default');
-        else $this->replyTo = $replyTo;
+        if ($replyTo != null) $this->replyTo = $replyTo;
     }
 
     public function setTo(array $to)
