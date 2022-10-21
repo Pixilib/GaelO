4ead88fe75d9c897cbbfcae7a069a5f751b24980
EmilieOLIVIE <emilie.olivie@hotmail.fr>
2022-10-21 10:11:47 +0200

    small fixes

--- GaelO2/app/GaelO/Services/MailServices.php
+++ GaelO2/app/GaelO/Services/MailServices.php
@@ -86,7 +86,7 @@ class MailServices
         }, $studiesEntities);
 
         $parameters = [
-            'name' => 'user',
+            'name' => 'User',
             'email' => $email,
             'studies' => $studies
         ];
@@ -117,7 +117,7 @@ class MailServices
     {
 
         $parameters = [
-            'name' => 'user',
+            'name' => 'User',
             'email' => $userEmail,
             'lastname' => $lastname,
             'firstname' => $firstname
@@ -135,7 +135,7 @@ class MailServices
     {
 
         $parameters = [
-            'name' => 'supervisor',
+            'name' => 'Supervisor',
             'study' => $studyName,
             'successList' => $successList,
             'failList' => $failList
@@ -153,7 +153,7 @@ class MailServices
     {
 
         $parameters = [
-            'name' => $this->getUserName($uploadUserId),
+            'name' => 'User',
             'study' => $studyName,
             'patientId' => $patientId,
             'patientCode' => $patientCode,
@@ -310,7 +310,7 @@ class MailServices
     {
 
         $parameters = [
-            'name' => 'supervisor',
+            'name' => 'Supervisor',
             'role' => $role,
             'study' => $studyName,
             'patientId' => $patientId,
@@ -335,7 +335,7 @@ class MailServices
     {
 
         $parameters = [
-            'name' => $this->getUserName($currentUserId),
+            'name' => 'reviewer',
             'study' => $studyName,
             'patientId' => $patientId,
             'patientCode' => $patientCode,
@@ -593,7 +593,7 @@ class MailServices
 
     public function sendCreatedUserMessage(string $email)
     {
-        $parameters = ['name' => 'user'];
+        $parameters = ['name' => 'User'];
 
         $this->mailInterface->setTo([$email]);
         $this->mailInterface->setReplyTo();
@@ -605,7 +605,14 @@ class MailServices
     public function sendMagicLink(int $targetedUserId, string $studyName, string $url, string $role, int $patientCode, string $visitType = null)
     {
 
-        $parameters = ['name' => 'user', 'study' => $studyName, 'url' => $url, 'role' => $role, 'patientCode' => $patientCode, 'visitType' => $visitType];
+        $parameters = [
+            'name' => 'User', 
+            'study' => $studyName, 
+            'url' => $url, 
+            'role' => $role, 
+            'patientCode' => $patientCode, 
+            'visitType' => $visitType
+        ];
 
         $this->mailInterface->setTo([$this->getUserEmail($targetedUserId)]);
         $this->mailInterface->setReplyTo($this->getStudyContactEmail($studyName));
