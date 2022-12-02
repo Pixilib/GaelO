cdc8d8f685563bbb33d1f2df4e7a00f0635ea21a
EmilieOLIVIE <emilie.olivie@hotmail.fr>
2022-12-02 12:39:05 +0100

    default exception message

--- GaelO2/app/GaelO/Exceptions/GaelOBadRequestException.php
+++ GaelO2/app/GaelO/Exceptions/GaelOBadRequestException.php
@@ -4,7 +4,7 @@ namespace App\GaelO\Exceptions;
 
 class GaelOBadRequestException extends AbstractGaelOException {
 
-    public function __construct(string $message) {
+    public function __construct(string $message = "Bad Request") {
         parent::__construct($message, 400, "Bad Request");
     }
 
--- GaelO2/app/GaelO/Exceptions/GaelOUnauthorizedException.php
+++ GaelO2/app/GaelO/Exceptions/GaelOUnauthorizedException.php
@@ -4,7 +4,7 @@ namespace App\GaelO\Exceptions;
 
 class GaelOUnauthorizedException extends AbstractGaelOException {
 
-    public function __construct(string $message) {
+    public function __construct(string $message = "Unauthorized") {
         parent::__construct($message, 401, "Unauthorized");
     }
 
