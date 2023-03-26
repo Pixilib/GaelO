d12c5d06864215e7a043ad5cf047999ee30df85e
Salim Kanoun <salim.kanoun@gmail.com>
2023-03-27 00:54:25 +0200

    add notificaiton read status and creation date

--- GaelO2/app/GaelO/Entities/NotificationEntity.php
+++ GaelO2/app/GaelO/Entities/NotificationEntity.php
@@ -6,6 +6,8 @@ class NotificationEntity {
     public string $id;
     public string $object;
     public string $message;
+    public bool $read;
+    public string $creationDate;
 
     public static function fillFromDBReponseArray(array $array): NotificationEntity
     {
@@ -13,6 +15,8 @@ class NotificationEntity {
         $documentationEntity->id = $array['id'];
         $documentationEntity->object = $array['data']['object'];
         $documentationEntity->message = $array['data']['message'];
+        $documentationEntity->read = $array['read_at'] ? true : false;
+        $documentationEntity->creationDate = $array['created_at'];
 
         return $documentationEntity;
     }
