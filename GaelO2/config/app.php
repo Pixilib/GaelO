6ebdd8f09315a06d9522ca52ad3d432fafd4a49c
Salim Kanoun <salim.kanoun@gmail.com>
2023-06-11 18:33:38 +0200

    2.0.5

--- GaelO2/config/app.php
+++ GaelO2/config/app.php
@@ -25,7 +25,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '2.0.4',
+    'version' => '2.0.5',
 
     /*
     |--------------------------------------------------------------------------
