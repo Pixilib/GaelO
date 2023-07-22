92b242389967a9524d2a09abcbdbae831e417f67
salim kanoun <salim.kanoun@gmail.Com>
2023-07-22 03:12:50 +0200

    fixed flysystem

--- GaelO2/app/GaelO/Adapters/FtpClientAdapter.php
+++ GaelO2/app/GaelO/Adapters/FtpClientAdapter.php
@@ -3,39 +3,54 @@
 namespace App\GaelO\Adapters;
 
 use App\GaelO\Exceptions\GaelOException;
-use DateTime;
 use App\GaelO\Interfaces\Adapters\FTPClientInterface;
-use League\Flysystem\Adapter\Ftp as FtpAdapter;
-use League\Flysystem\Sftp\SftpAdapter;
 use League\Flysystem\Filesystem;
-use League\Flysystem\FilesystemInterface;
+use League\Flysystem\PhpseclibV3\SftpAdapter;
+use League\Flysystem\PhpseclibV3\SftpConnectionProvider;
+use League\Flysystem\Ftp\FtpAdapter;
+use League\Flysystem\Ftp\FtpConnectionOptions;
+use DateTime;
+use Exception;
 
 class FtpClientAdapter implements FTPClientInterface
 {
 
-    private FilesystemInterface $filesystem;
+    private Filesystem $filesystem;
 
     public function setFTPServer(string $host, int $port, ?string $username, ?string $password, bool $isSftp, bool $isSSL): void
     {
         if (!$isSftp) {
-
-            $this->filesystem = new Filesystem(new FtpAdapter([
-                'host' => $host,
-                'username' => $username,
-                'password' => $password,
-                'port' => $port,
-                'ssl' => $isSSL,
-                'timeout' => 30
-            ]));
+            $this->filesystem = new FileSystem(
+                new FtpAdapter(
+                    // Connection options
+                    FtpConnectionOptions::fromArray([
+                        'host' => $host, // required
+                        'root' => '/', // required
+                        'username' => $username, // required
+                        'password' => $password, // required
+                        'port' => $port,
+                        'ssl' => $isSSL,
+                        'timeout' => 30,
+                    ])
+                )
+            );
         } else {
 
-            $this->filesystem = new Filesystem(new SftpAdapter([
-                'host' => $host,
-                'username' => $username,
-                'password' => $password,
-                'port' => $port,
-                'timeout' => 30,
-            ]));
+            $this->filesystem = new Filesystem(
+                new SftpAdapter(
+                    new SftpConnectionProvider(
+                        $host,
+                        $username,
+                        $password,
+                        null,
+                        null,
+                        $port,
+                        false,
+                        30
+                    ),
+                    '/'
+                )
+            );
         }
     }
 
@@ -45,7 +60,7 @@ class FtpClientAdapter implements FTPClientInterface
             throw new GaelOException('FTP File Not Found');
         }
 
-        $lastUpdateTimeStamp = $this->filesystem->getTimestamp($fullPath);
+        $lastUpdateTimeStamp = $this->filesystem->lastModified($fullPath);
         $dateNow = new DateTime();
 
         if ($maxAgeSeconds && ($dateNow->getTimestamp() - $lastUpdateTimeStamp) > $maxAgeSeconds) {
@@ -59,10 +74,11 @@ class FtpClientAdapter implements FTPClientInterface
 
     public function writeFileContent(string $content, string $destinationPath): bool
     {
-        $success = $this->filesystem->put($destinationPath, $content);
-        if (!$success) {
-            throw new GaelOException('FTP Write Error');
+        try {
+            $this->filesystem->write($destinationPath, $content);
+        } catch (Exception $e) {
+            return false;
         }
-        return $success;
+        return true;
     }
 }
