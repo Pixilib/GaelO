699bd8af80bd8c6ff6186b1ac92ddc5458f3a799
Salim Kanoun <salim.kanoun@gmail.com>
2023-09-21 22:48:24 +0200

    upgrade version

--- GaelO2/config/app.php
+++ GaelO2/config/app.php
@@ -25,7 +25,7 @@ return [
     | or any other location as required by the application or its packages.
     */
 
-    'version' => '2.1.1',
+    'version' => '2.2.0',
 
     /*
     |--------------------------------------------------------------------------
