6655bfd06ca7566dfe1e4e7c3a29d6c7013e4035
Salim Kanoun <salim.kanoun@gmail.com>
2023-10-10 00:13:49 +0200

    cehck message

--- GaelO2/app/GaelO/views/mails/mail_radiomics_report.blade.php
+++ GaelO2/app/GaelO/views/mails/mail_radiomics_report.blade.php
@@ -167,7 +167,7 @@
         </tbody>
       </table>
     </div>
-    <!--[if mso | IE]></td></tr></table><![endif]--> @if($message!=null)
+    <!--[if mso | IE]></td></tr></table><![endif]--> @if(!empty($message))
     <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
     <div style="margin:0px auto;max-width:600px;">
       <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
