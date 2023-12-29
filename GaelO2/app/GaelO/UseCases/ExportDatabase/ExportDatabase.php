f095f3de3897e0fdb11dbe649741b5424865d26d
salim kanoun <salim.kanoun@gmail.Com>
2023-12-29 10:25:16 +0100

    max execution time for export apis

--- GaelO2/app/GaelO/UseCases/ExportDatabase/ExportDatabase.php
+++ GaelO2/app/GaelO/UseCases/ExportDatabase/ExportDatabase.php
@@ -27,7 +27,8 @@ class ExportDatabase
 
         try {
             $this->checkAuthorization($exportDatabaseRequest->currentUserId);
-
+            //Operation might be long, set max execution time to 30 minutes
+            set_time_limit(1800);
             $zip = new ZipArchive();
             $tempZip = tempnam(ini_get('upload_tmp_dir'), 'TMPZIPDB_');
             $zip->open($tempZip, ZipArchive::OVERWRITE);
