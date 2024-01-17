8ffab1fe7e0a61dc11ebeb73cf53820f1649ed4b
salim kanoun <salim.kanoun@gmail.Com>
2024-01-17 16:48:06 +0100

    try fix test

--- GaelO2/tests/Unit/TestServices/PdfServiceTest.php
+++ GaelO2/tests/Unit/TestServices/PdfServiceTest.php
@@ -18,7 +18,7 @@ class PdfServiceTest extends TestCase
 
     public function testMakeRadiomicsPdf()
     {
-        $filename = $this->pdfService->saveRadiomicsPdf('TEST', '12345', 'PET0', '07141995', ['tmtv41' => 55]);
+        $filename = $this->pdfService->saveRadiomicsPdf('TEST', '12345', 'PET0', '07141995', ['tmtv41' => 55, 'magicLink'=> 'link']);
         $this->assertIsString($filename);
     }
 
