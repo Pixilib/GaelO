4f6b578d4e4ff5e9513e94c4a56ef90fc4c2a156
Salim Kanoun <salim.kanoun@gmail.com>
2024-03-03 14:37:49 +0100

    expected patient now could have a null center

--- GaelO2/app/GaelO/Services/GaelOStudiesService/ExpectedPatient/ExpectedPatient.php
+++ GaelO2/app/GaelO/Services/GaelOStudiesService/ExpectedPatient/ExpectedPatient.php
@@ -5,10 +5,10 @@ namespace App\GaelO\Services\GaelOStudiesService\ExpectedPatient;
 class ExpectedPatient
 {
     public string $code;
-    public int $centerCode;
+    public ?int $centerCode;
     public ?string $inclusionStatus;
 
-    public function __construct(string $code, int $centerCode = null, ?string $inclusionStatus = null)
+    public function __construct(string $code, ?int $centerCode = null, ?string $inclusionStatus = null)
     {
         $this->code = $code;
         $this->centerCode = $centerCode;
