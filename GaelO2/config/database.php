b4ad0422c8a53c3516e1876429397d387cb7d672
salim kanoun <salim.kanoun@gmail.Com>
2024-03-30 23:32:05 +0100

    add REDIS_SCHEME

--- GaelO2/config/database.php
+++ GaelO2/config/database.php
@@ -133,6 +133,7 @@ return [
         ],
 
         'default' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
@@ -141,6 +142,7 @@ return [
         ],
 
         'cache' => [
+            'scheme' => env('REDIS_SCHEME', 'tcp'),
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
