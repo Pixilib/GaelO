a9b7be263df7cb67b7e36bc65f7a1fe652013f56
Salim Kanoun <salim.kanoun@gmail.com>
2024-11-27 08:48:45 +0100

    try export database as stream

--- GaelO2/app/GaelO/UseCases/ExportDatabase/ExportDatabase.php
+++ GaelO2/app/GaelO/UseCases/ExportDatabase/ExportDatabase.php
@@ -2,13 +2,13 @@
 
 namespace App\GaelO\UseCases\ExportDatabase;
 
+use App\GaelO\Adapters\FrameworkAdapter;
+use App\GaelO\Adapters\ZipStreamAdapter;
 use App\GaelO\Exceptions\AbstractGaelOException;
 use App\GaelO\Exceptions\GaelOForbiddenException;
 use App\GaelO\Interfaces\Adapters\DatabaseDumperInterface;
 use App\GaelO\Services\AuthorizationService\AuthorizationUserService;
-use App\GaelO\Util;
 use Exception;
-use ZipArchive;
 
 class ExportDatabase
 {
@@ -29,24 +29,9 @@ class ExportDatabase
             $this->checkAuthorization($exportDatabaseRequest->currentUserId);
             //Operation might be long, set max execution time to 30 minutes
             set_time_limit(1800);
-            $zip = new ZipArchive();
-            $tempZip = tempnam(ini_get('upload_tmp_dir'), 'TMPZIPDB_');
-            $zip->open($tempZip, ZipArchive::OVERWRITE);
-
-            $filePathSql = tempnam(ini_get('upload_tmp_dir'), 'TMPDB_');
-            $this->databaseDumperInterface->createDatabaseDumpFile($filePathSql);
-
-            $date = Date('Ymd_His');
-            $zip->addFile($filePathSql, "export_database_$date.sql");
-            $zip->close();
-            
-            //Unlick after lock released by zip close
-            unlink($filePathSql);
 
             $exportDatabaseResponse->status = 200;
             $exportDatabaseResponse->statusText = 'OK';
-            $exportDatabaseResponse->zipFile = $tempZip;
-            $exportDatabaseResponse->fileName = "export_database_" . $date . ".zip";
         } catch (AbstractGaelOException $e) {
             $exportDatabaseResponse->status = $e->statusCode;
             $exportDatabaseResponse->statusText = $e->statusText;
@@ -60,6 +45,27 @@ class ExportDatabase
         $this->authorizationUserService->setUserId($userId);
         if (!$this->authorizationUserService->isAdmin($userId)) {
             throw new GaelOForbiddenException();
+        }
+    }
+
+    public function readExport()
+    {
+        $date = Date('Ymd_His');
+        $zipStream = new ZipStreamAdapter();
+        $zipStream->init("export_database_" . $date . ".zip");
+        //send stored file for this study
+        try {
+            $filePathSql = tempnam(ini_get('upload_tmp_dir'), 'TMPDB_');
+            $this->databaseDumperInterface->createDatabaseDumpFile($filePathSql);
+            $zipStream->addFileFromStream(fopen($filePathSql, 'rb'), "export_database_$date.sql");
+            $files = FrameworkAdapter::getStoredFiles();
+            foreach ($files as $file) {
+                $fileStream = FrameworkAdapter::getFile($file, true);
+                $zipStream->addFileFromStream($file, $fileStream);
+            }
+            $zipStream->finish();
+        } finally {
+            unlink($filePathSql);
         }
     }
 }
