7f5a2339a0b53050d0622f15b801da324b1d98e6
Salim Kanoun <salim.kanoun@gmail.com>
2024-12-02 00:51:00 +0100

    increased memory limit

--- GaelO2/app/GaelO/UseCases/ExportStudyData/ExportStudyData.php
+++ GaelO2/app/GaelO/UseCases/ExportStudyData/ExportStudyData.php
@@ -31,6 +31,8 @@ class ExportStudyData
 
             //Make this task continues even the users leave to prevent removing created temporary files
             ignore_user_abort(true);
+            //Increased memory limit
+            ini_set('memory_limit', '1024M');
             //Operation might be long, set max execution time to 30 minutes
             set_time_limit(1800);
 
