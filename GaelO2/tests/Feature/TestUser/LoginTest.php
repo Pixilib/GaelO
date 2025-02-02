a7e89b0dc243f3e3fe53acef9d885f9d584aa5c5
Salim Kanoun <salim.kanoun@gmail.com>
2025-02-02 22:15:30 +0100

    test enhancments

--- GaelO2/tests/Feature/TestUser/LoginTest.php
+++ GaelO2/tests/Feature/TestUser/LoginTest.php
@@ -10,6 +10,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Config;
 use Mockery;
 use Mockery\MockInterface;
+use Tests\AuthorizationTools;
 
 class LoginTest extends TestCase
 {
@@ -30,39 +31,45 @@ class LoginTest extends TestCase
      */
     public function testLogin()
     {
-        $data = ['email'=> 'administrator@gaelo.fr',
-        'password'=> 'administrator'];
+        $data = [
+            'email' => 'administrator@gaelo.fr',
+            'password' => 'administrator'
+        ];
         $adminDefaultUser = User::where('id', 1)->first();
-        $adminDefaultUser->onboarding_version= Config::get('app.onboarding_version');
+        $adminDefaultUser->onboarding_version = Config::get('app.onboarding_version');
         $adminDefaultUser->save();
-        $response = $this->json('POST', '/api/login', $data)-> assertSuccessful();
-        $content= json_decode($response->content(), true);
+        $response = $this->json('POST', '/api/login', $data)->assertSuccessful();
+        $content = json_decode($response->content(), true);
         $this->assertArrayHasKey('access_token', $content);
         $this->assertEquals($content['onboarded'], true);
     }
 
     public function testLoginShouldPassInsensitive()
     {
-        $data = ['email'=> strtoupper('administrator@gaelo.fr'),
-        'password'=> 'administrator'];
+        $data = [
+            'email' => strtoupper('administrator@gaelo.fr'),
+            'password' => 'administrator'
+        ];
         $adminDefaultUser = User::where('id', 1)->first();
-        $adminDefaultUser->onboarding_version= Config::get('app.onboarding_version');
+        $adminDefaultUser->onboarding_version = Config::get('app.onboarding_version');
         $adminDefaultUser->save();
-        $response = $this->json('POST', '/api/login', $data)-> assertSuccessful();
-        $content= json_decode($response->content(), true);
+        $response = $this->json('POST', '/api/login', $data)->assertSuccessful();
+        $content = json_decode($response->content(), true);
         $this->assertArrayHasKey('access_token', $content);
         $this->assertEquals($content['onboarded'], true);
     }
 
     public function testLoginSuccessButNotOnboarded()
     {
-        $data = ['email'=> 'administrator@gaelo.fr',
-        'password'=> 'administrator'];
+        $data = [
+            'email' => 'administrator@gaelo.fr',
+            'password' => 'administrator'
+        ];
         $adminDefaultUser = User::where('id', 1)->first();
         $adminDefaultUser->onboarding_version = '0.0.0';
         $adminDefaultUser->save();
-        $response = $this->json('POST', '/api/login', $data)-> assertSuccessful();
-        $content= json_decode($response->content(), true);
+        $response = $this->json('POST', '/api/login', $data)->assertSuccessful();
+        $content = json_decode($response->content(), true);
         $this->assertArrayHasKey('access_token', $content);
         $this->assertEquals($content['onboarded'], false);
     }
@@ -70,8 +77,10 @@ class LoginTest extends TestCase
 
     public function testLoginNonExistingUser()
     {
-        $data = ['email'=> 'administrator2@gaelo.fr',
-        'password'=> 'administrator'];
+        $data = [
+            'email' => 'administrator2@gaelo.fr',
+            'password' => 'administrator'
+        ];
         $adminDefaultUser = User::where('id', 1)->first();
         $adminDefaultUser->save();
         $this->json('POST', '/api/login', $data)->assertStatus(401);
@@ -79,8 +88,10 @@ class LoginTest extends TestCase
 
     public function testLoginWrongPassword()
     {
-        $data = ['email'=> 'administrator@gaelo.fr',
-        'password'=> 'wrongPassword'];
+        $data = [
+            'email' => 'administrator@gaelo.fr',
+            'password' => 'wrongPassword'
+        ];
         $adminDefaultUser = User::where('id', 1)->first();
         $adminDefaultUser->save();
         $this->json('POST', '/api/login', $data)->assertStatus(401);
@@ -89,29 +100,36 @@ class LoginTest extends TestCase
     public function testLoginShouldFailBecauseUnconfirmedAccound()
     {
         //Try with correct main password but user in unconfirmed status, should fail
-        $data = ['email'=> 'administrator@gaelo.fr',
-        'password'=> 'administrator'];
+        $data = [
+            'email' => 'administrator@gaelo.fr',
+            'password' => 'administrator'
+        ];
         $adminDefaultUser = User::where('id', 1)->first();
         $adminDefaultUser['email_verified_at'] = null;
         $adminDefaultUser->save();
         $this->json('POST', '/api/login', $data)->assertStatus(401);
     }
 
-    public function testAccountBlocked(){
+    public function testAccountBlocked()
+    {
         //Access should be forbidden even if credential correct because of blocker status
-        $data = ['email'=> 'administrator@gaelo.fr',
-        'password'=> 'administrator'];
+        $data = [
+            'email' => 'administrator@gaelo.fr',
+            'password' => 'administrator'
+        ];
         $adminDefaultUser = User::where('id', 1)->first();
         $adminDefaultUser['attempts'] = 3;
         $adminDefaultUser->save();
         $this->json('POST', '/api/login', $data)->assertStatus(401);
-
     }
 
-    public function testBlokingAccount(){
+    public function testBlokingAccount()
+    {
         // Three wrong attempts to login should block account
-        $data = ['email'=> 'administrator@gaelo.fr',
-        'password'=> 'wrongPassword'];
+        $data = [
+            'email' => 'administrator@gaelo.fr',
+            'password' => 'wrongPassword'
+        ];
 
         $this->json('POST', '/api/login', $data)->assertStatus(401);
         $this->json('POST', '/api/login', $data)->assertStatus(401);
@@ -120,7 +138,11 @@ class LoginTest extends TestCase
         $adminDefaultUser = User::where('id', 1)->first();
         $this->assertEquals($adminDefaultUser['attempts'], 3);
         $this->trackerSpy->shouldHaveReceived('writeAction')->once()->with(1, Constants::TRACKER_ROLE_USER, Mockery::any(), Mockery::any(), Constants::TRACKER_ACCOUNT_BLOCKED, Mockery::any());
-
     }
 
+    public function testLogout()
+    {
+        AuthorizationTools::actAsAdmin(true);
+        $this->json('DELETE', '/api/login')->assertSuccessful();
+    }
 }
