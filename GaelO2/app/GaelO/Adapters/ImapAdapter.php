a3cac49495f581945f11ad5b9828cde72c2c7a6d
Salim Kanoun <salim.kanoun@gmail.com>
2026-01-17 23:57:20 +0100

    init imap engine

--- GaelO2/app/GaelO/Adapters/ImapAdapter.php
+++ GaelO2/app/GaelO/Adapters/ImapAdapter.php
@@ -2,28 +2,58 @@
 
 namespace App\GaelO\Adapters;
 
+use DirectoryTree\ImapEngine\Collections\MessageCollection;
 use DirectoryTree\ImapEngine\Mailbox;
 
-use App\GaelO\Interfaces\Adapters\ImapInterface;
-use App\Jobs\JobQcReport;
-use Illuminate\Support\Facades\Mail;
+use DirectoryTree\ImapEngine\Laravel\Facades\Imap;
+use DirectoryTree\ImapEngine\Exceptions\ImapCommandException;
+use DirectoryTree\ImapEngine\Exceptions\ImapConnectionException;
+use Illuminate\Support\Facades\Log;
 
-class ImapAdapter implements ImapInterface
+class ImapAdapter
 {
     private Mailbox $mailbox;
-    public function sendQcReportJob(int $visitId): void
+    private MessageCollection $messages;
+
+    public function connect($mailbox = 'default'): void
     {
-        $this->mailbox = new Mailbox([
-            'host' => 'imap.example.com',
-            'port' => 993,
-            'encryption' => 'ssl',
-            'username' => 'user@example.com',
-            'password' => 'password',
-        ]);
+        $this->mailbox = Imap::mailbox($mailbox);
     }
     public function getMessages()
     {
-        $inbox = $this->mailbox->inbox();
-        $messages = $inbox->messages()->get();
+        try {
+            $this->mailbox->connect();
+            $inbox = $this->mailbox->inbox();
+            $this->messages = $inbox->messages()->withBody()->withHeaders()->get();
+        } catch (ImapCommandException $e) {
+            Log::error('IMAP Command Exception: ' . $e->getMessage());
+        } catch (ImapConnectionException $e) {
+            Log::error('IMAP Connection Exception: ' . $e->getMessage());
+        }
+    }
+
+    public function isEmpty(): bool
+    {
+        return $this->messages->count() === 0;
+    }
+
+    public function getMessage(bool $markAsSeen)
+    {
+        foreach ($this->messages as $message) {
+            $header = $message->headers();
+            $body = $message->body();
+            $to = $message->to()[0]->email();
+            if ($markAsSeen) $message->markSeen();
+            yield [
+                'to' => $to,
+                'header' => $header,
+                'body' => $body,
+            ];
+        }
+    }
+
+    public function disconnect(): void
+    {
+        $this->mailbox->disconnect();
     }
 }
