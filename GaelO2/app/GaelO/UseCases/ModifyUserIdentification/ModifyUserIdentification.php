f4b60257cc872e99ab38dc197333d7ce6e581bc2
Neo <contact@pixilib.fr>
2026-06-18 15:58:49 +0200

    front correction

--- GaelO2/app/GaelO/UseCases/ModifyUserIdentification/ModifyUserIdentification.php
+++ GaelO2/app/GaelO/UseCases/ModifyUserIdentification/ModifyUserIdentification.php
@@ -56,7 +56,7 @@ class ModifyUserIdentification
                 $modifyUserIdentificationRequest->lastname,
                 $modifyUserIdentificationRequest->firstname,
                 $modifyUserIdentificationRequest->email,
-                $user['disable_email_notification'],
+                $modifyUserIdentificationRequest->disable_email_notification,
                 $modifyUserIdentificationRequest->phone,
                 $user['administrator'],
                 $user['center_code'],
--- GaelO2/app/GaelO/UseCases/ModifyUserIdentification/ModifyUserIdentificationRequest.php
+++ GaelO2/app/GaelO/UseCases/ModifyUserIdentification/ModifyUserIdentificationRequest.php
@@ -10,4 +10,5 @@ class ModifyUserIdentificationRequest
     public ?string $firstname = null;
     public ?string $phone = null;
     public string $email;
+    public ?bool $disable_email_notification = null;
 }
