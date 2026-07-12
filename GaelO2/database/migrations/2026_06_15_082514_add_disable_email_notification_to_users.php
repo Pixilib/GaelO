94b45c85dad01aff38d72c091eac1d0d5f538389
Salim Kanoun <salim.kanoun@gmail.com>
2026-07-12 21:10:36 +0200

    email notif not nullable

--- GaelO2/database/migrations/2026_06_15_082514_add_disable_email_notification_to_users.php
+++ GaelO2/database/migrations/2026_06_15_082514_add_disable_email_notification_to_users.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->boolean('enable_email_notifications')
                 ->default(true)
+                ->nullable(false)
                 ->after('email');
         });
     }
