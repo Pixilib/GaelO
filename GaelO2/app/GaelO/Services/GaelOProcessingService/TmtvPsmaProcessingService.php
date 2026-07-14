9b3f325028f8d4ab1ae03efee5b856976482391f
salim kanoun <salim.kanoun@gmail.com>
2026-07-14 10:31:12 +0200

    use default version if not specified,  model name can be changed in TMTVProcessingService

--- GaelO2/app/GaelO/Services/GaelOProcessingService/TmtvPsmaProcessingService.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\GaelO\Services\GaelOProcessingService;
-
-class TmtvPsmaProcessingService extends TmtvProcessingService
-{
-    protected string $modelName = 'pt_seg_swinunetr_psma';
-}
